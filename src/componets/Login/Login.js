8cc9b15ffa45a5b7b7fff0ad8f6b01c31028bd99
Zunaid-jony <jony35666666@gmail.com>
2022-05-11 11:30:43 +0600

    login

--- src/componets/Login/Login.js
+++ src/componets/Login/Login.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import './Login.css'
+
+import Img from '../../componets/images/logo2.png'
 
 const Login = () => {
     return (
-        <div>
-           <h1>
-               This is login pages
-           </h1>
-           <h1>
-               This is login pages
-           </h1><h1>
-               This is login pages
-           </h1><h1>
-               This is login pages
-           </h1>
-        </div>
+        <section className='login d-flex align-items-center text-center'>
+           <div>
+
+           </div>
+            <div className='container '>
+                <img className='logos' src={Img}></img>
+                <br></br>
+                <br></br>
+                <input className='button' placeholder=' Name'></input>
+                <br></br>
+                <br></br>
+                <input  className='button' placeholder='Email'></input>
+                <br></br>
+                <br></br>
+                <input className='button' placeholder='Password'></input>
+                <br></br>
+                <br></br>
+                <input className='button' placeholder='Confirm Password'></input>
+                <br></br>
+                <br></br>
+                <input className='button  button1' placeholder='Sign up'></input>
+                
+
+               
+            </div>
+        </section>
     );
 };
 
